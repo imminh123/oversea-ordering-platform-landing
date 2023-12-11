dd4cbb1c520ea868776fa8f69d8184889fc6be49
Minh Nguyen <minhco12@gmail.com>
2023-12-11 16:23:10 +0200

    update meta

--- pages/index.js
+++ pages/index.js
@@ -21,15 +21,34 @@ export default function Home() {
           name="description"
           content="Nhập Hàng Trung Quốc Tận Gốc, Tối Ưu Chi Phí, Vận Chuyển Nhanh Chóng"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <meta
+          property="og:image"
+          content="http://graphics.myfavnews.com/images/logo-100x100.jpg"
+        />
+          <meta name="keywords" content="logistics, vận chuyển, vận chuyển trung quốc, mua hàng Taobao, logistics Việt Nam, MBY Logistics"/>
+          <meta name="author" content="MBY Logistics"/>
+          <meta name="robots" content="index, follow"/>
+          <meta property="og:title" content="MBY Logistics"/>
+          <meta property="og:description" content="Nhập Hàng Trung Quốc Tận Gốc, Tối Ưu Chi Phí, Vận Chuyển Nhanh Chóng"/>
+          <meta property="og:type" content="website"/>
+          <meta property="og:url" content="https://www.mby.vn/"/>
+          <meta property="og:image" content="https://i.ibb.co/M5VxGWM/mby-logo-rec-upscale.png"/>
+          <meta name="twitter:card" content="summary_large_image"/>
+          <meta name="twitter:title" content="MBY Logistics"/>
+          <meta name="twitter:description" content="Nhập Hàng Trung Quốc Tận Gốc, Tối Ưu Chi Phí, Vận Chuyển Nhanh Chóng"/>
+          <meta name="twitter:image" content="https://i.ibb.co/M5VxGWM/mby-logo-rec-upscale.png"></meta>
+          <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Navbar />
       <Hero />
       <SectionTitle
         pretitle="Ưu Điểm"
-        title="Tại sao bạn nên sử dụng dịch vụ của MBY Logistics">
-        MBY Logistics - nền tảng đơn giản hóa mua hàng quốc tế, chuyên vận chuyển hàng từ Trung Quốc đến Việt Nam đảm bảo giải pháp hiệu quả về chi phí, xử lý đơn hàng nhanh chóng và dịch vụ vận chuyển tin cậy.
+        title="Tại sao bạn nên sử dụng dịch vụ của MBY Logistics"
+      >
+        MBY Logistics - nền tảng đơn giản hóa mua hàng quốc tế, chuyên vận
+        chuyển hàng từ Trung Quốc đến Việt Nam đảm bảo giải pháp hiệu quả về chi
+        phí, xử lý đơn hàng nhanh chóng và dịch vụ vận chuyển tin cậy.
       </SectionTitle>
       <Benefits data={benefitOne} />
       <Benefits imgPos="right" data={benefitTwo} />
@@ -43,12 +62,15 @@ export default function Home() {
       <Video /> */}
       <SectionTitle
         pretitle="Đánh Giá"
-        title="Phản hồi từ khách hàng của chúng tôi">
-         Sự hài lòng được thể hiện qua đánh giá của khách hàng đối với chất lượng và dịch vụ của MBY Logistics
+        title="Phản hồi từ khách hàng của chúng tôi"
+      >
+        Sự hài lòng được thể hiện qua đánh giá của khách hàng đối với chất lượng
+        và dịch vụ của MBY Logistics
       </SectionTitle>
       <Testimonials />
       <SectionTitle pretitle="FAQ" title="Câu Hỏi Thường Gặp">
-        Một vài câu hỏi thường gặp của khách hàng khi trải nghiệm dịch vụ của MBY Logistics
+        Một vài câu hỏi thường gặp của khách hàng khi trải nghiệm dịch vụ của
+        MBY Logistics
       </SectionTitle>
       <Faq />
       <Cta />
